03672a158b3a0a6196d56606f00b1ce8c28ed08f
Joshun <joshua.oleary@btinternet.com>
2014-11-28 20:17:39 +0000

    Correct map indexing in draw_map

--- battleships.rb
+++ battleships.rb
@@ -16,9 +16,16 @@ end
 
 
 def draw_map(array)
-	array.each do |row|
-		row.each do |column|
-			print column
+	for row in 0..(array.length - 1)
+		if row == 0
+			puts "  0 1 2 3 4 5 6 7 8 9".colorize(:red)
+		end
+		
+		for column in 0..(array[row].length - 1)
+			if column == 0
+				print row.to_s.colorize(:red) + " "
+			end
+			print array[row][column] + " "
 		end
 		puts ""
 	end
@@ -26,7 +33,7 @@ end
 
 def main()
 	puts "Creating new battleship grid of (" + GRID_SIZE.to_s + "x" + GRID_SIZE.to_s + ") squares."
-	battleGrid = Array.new(GRID_SIZE) { Array.new(GRID_SIZE, 0) }
+	battleGrid = Array.new(GRID_SIZE) { Array.new(GRID_SIZE, "w") }
 	
 	draw_map(battleGrid)
 		
