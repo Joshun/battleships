87ba360e6f1ecf3359c92473400f82175090f4a0
Joshun <joshua.oleary@btinternet.com>
2014-11-28 19:51:39 +0000

    First commit

---
+++ battleships.rb
@@ -0,0 +1,26 @@
+# Battleships
+#
+# Joshua O'Leary
+# Assignment 2
+# 12/2014
+# University of Sheffield
+
+require "colorize"
+
+
+GRID_SIZE = 10
+
+def print_coloured(str, colour)
+	puts str.colorize(colour)
+end
+
+
+def main()
+	puts "Creating new battleship grid of (" + GRID_SIZE.to_s + "x" + GRID_SIZE.to_s + ") squares."
+	battleGrid = Array.new(GRID_SIZE) { Array.new(GRID_SIZE, 0) }
+	
+	draw_map(battleGrid)
+		
+end
+
+main
